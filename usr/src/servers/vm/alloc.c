36a9447d2393c4f08ac6eb1016fba53a23694d7a
hanshenrik <hh.gronsleth@gmail.com>
2014-11-26 11:13:23 +0000

    removed do

--- usr/src/servers/vm/alloc.c
+++ usr/src/servers/vm/alloc.c
@@ -215,76 +215,74 @@ PUBLIC phys_clicks alloc_mem_f(phys_clicks clicks, u32_t memflags)
 	mem = alloc_pages(clicks, memflags);
   } else {
 CHECKHOLES;
-  
-    do {
-      prev_ptr = NIL_HOLE;
-    	hp = hole_head;
-      switch(alloc_algorithm) {
-        default:
-        case(0): /* First fit */
-          while (hp != NIL_HOLE) {
-            if (hp->h_len >= clicks) { /* We found a hole that is big enough. Use it. */
-              old_base = hp->h_base;  /* remember where it started */
-              hp->h_base += clicks; /* bite a piece off */
-              hp->h_len -= clicks;  /* ditto */
 
-              /* Delete the hole if used up completely. */
-              if (hp->h_len == 0) del_slot(prev_ptr, hp);
+    prev_ptr = NIL_HOLE;
+    hp = hole_head;
+    switch(alloc_algorithm) {
+      default:
+      case(0): /* First fit */
+        while (hp != NIL_HOLE) {
+          if (hp->h_len >= clicks) { /* We found a hole that is big enough. Use it. */
+            old_base = hp->h_base;  /* remember where it started */
+            hp->h_base += clicks; /* bite a piece off */
+            hp->h_len -= clicks;  /* ditto */
 
-              /* Anything special needs to happen? */
-              if(memflags & PAF_CLEAR) {
-                if ((s= sys_memset(0, CLICK_SIZE*old_base,
-                CLICK_SIZE*clicks)) != OK)   {
-                vm_panic("alloc_mem: sys_memset failed", s);
-                }
+            /* Delete the hole if used up completely. */
+            if (hp->h_len == 0) del_slot(prev_ptr, hp);
+
+            /* Anything special needs to happen? */
+            if(memflags & PAF_CLEAR) {
+              if ((s= sys_memset(0, CLICK_SIZE*old_base,
+              CLICK_SIZE*clicks)) != OK)   {
+              vm_panic("alloc_mem: sys_memset failed", s);
               }
-
-              /* Return the start address of the acquired block. */
-              CHECKHOLES;
-              mem = old_base;
-              break;
             }
 
-            prev_ptr = hp;
-            hp = hp->h_next;
+            /* Return the start address of the acquired block. */
+            CHECKHOLES;
+            mem = old_base;
+            break;
           }
-          break;
-        case(1): /* Best fit */
-          candidate_hole = hp;
+
           prev_ptr = hp;
           hp = hp->h_next;
-          while (hp != NIL_HOLE) {
-            if (hp->h_len >= clicks && hp->h_len < candidate_hole->h_len) {
-              candidate_hole = hp; /* or using pointer? */
-            }
-
-            prev_ptr = hp;
-            hp = hp->h_next;
-          }
-          /* we've gone through the whole list, candidate_hole is now the smallest big enough hole */
-          old_base = candidate_hole->h_base;  /* remember where it started */
-          candidate_hole->h_base += clicks; /* bite a piece off */
-          candidate_hole->h_len -= clicks;  /* ditto */
-          
-          /* Delete the hole if used up completely. */
-          if (candidate_hole->h_len == 0) del_slot(prev_ptr, candidate_hole);
-
-          /* Anything special needs to happen? */
-          if(memflags & PAF_CLEAR) {
-            if ((s= sys_memset(0, CLICK_SIZE*old_base,
-            CLICK_SIZE*clicks)) != OK)   {
-            vm_panic("alloc_mem: sys_memset failed", s);
-            }
+        }
+        break;
+      case(1): /* Best fit */
+        candidate_hole = hp;
+        prev_ptr = hp;
+        hp = hp->h_next;
+        while (hp != NIL_HOLE) {
+          if (hp->h_len >= clicks && hp->h_len < candidate_hole->h_len) {
+            candidate_hole = hp; /* or using pointer? */
           }
 
-          /* Return the start address of the acquired block. */
-          CHECKHOLES;
-          mem = old_base;
-          
-          break;
-        case(2): /* Quick fit */
-          break;
-      }
+          prev_ptr = hp;
+          hp = hp->h_next;
+        }
+        /* we've gone through the whole list, candidate_hole is now the smallest big enough hole */
+        old_base = candidate_hole->h_base;  /* remember where it started */
+        candidate_hole->h_base += clicks; /* bite a piece off */
+        candidate_hole->h_len -= clicks;  /* ditto */
+        
+        /* Delete the hole if used up completely. */
+        if (candidate_hole->h_len == 0) del_slot(prev_ptr, candidate_hole);
+
+        /* Anything special needs to happen? */
+        if(memflags & PAF_CLEAR) {
+          if ((s= sys_memset(0, CLICK_SIZE*old_base,
+          CLICK_SIZE*clicks)) != OK)   {
+          vm_panic("alloc_mem: sys_memset failed", s);
+          }
+        }
+
+        /* Return the start address of the acquired block. */
+        CHECKHOLES;
+        mem = old_base;
+        
+        break;
+      case(2): /* Quick fit */
+        break;
     }
   }
 
